96e3919ddf0bd8ece1cd53def56979afee261bd2
kartikDas074 <kartikdas209774@gmail.com>
2026-03-28 21:43:11 +0600

    setup the project environment

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Country from './Country'
+function App() {
+  
+
+  return (
+    <>
+    
+    <h1>Nothing goes as planed in this accused world</h1>
+    <Country></Country>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/Country.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Country = () => {
+    return (
+        <div>
+            <h1>tre xudi</h1>
+        </div>
+    );
+};
+
+export default Country;
